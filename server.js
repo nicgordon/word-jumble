696a9cc783efed0cfbdce8a4ccfb97808d2cafaa
Nic Gordon <gordonna@gmail.com>
2015-09-15 08:37:26 +1000

    Server needs to serve dist dir

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var app = express();
 var path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.use('/dist', express.static('dist'));
 
 var server = app.listen(3333, function () {
   var host = server.address().address;
