9394c020582bedbb507a316d575502b2107204b3
Nic Gordon <gordonna@gmail.com>
2015-09-15 08:38:19 +1000

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+var path = require('path');
+
 module.exports = {
-    entry: "./src/app",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    }
+  entry: "./src",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel?stage=0',
+    }],
+  },
+  resolve: {
+    root: __dirname,
+    modulesDirectories: [ 'node_modules' ],
+    extensions: [ '', '.js', '.jsx' ],
+    alias: {},
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+    alias: {},
+  }
 };
