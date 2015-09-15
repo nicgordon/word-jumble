ca0f3c3e27eebfe7f98e3f6e3ca3152996059c2c
Nic Gordon <gordonna@gmail.com>
2015-09-16 08:50:50 +1000

    State contains collections of tiles

--- src/reducers/tiles.js
+++ src/reducers/tiles.js
@@ -1,8 +1,16 @@
 import { CLEAR_SELECTED } from '../actions/action-types';
 
 const initialState = {
-  collection: [],
-  selected: [],
+  jumble: [
+    { value: 'c' },
+    { value: 'h' },
+    { value: 'i' },
+    { value: 'c' },
+    { value: 'k' },
+    { value: 'e' },
+    { value: 'n' },
+  ],
+  solution: [],
 };
 
 export default function todos(state = initialState, action) {
