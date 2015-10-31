e253ba7f183395bd88bfe874b21da08da58345a3
Nic Gordon <gordonna@gmail.com>
2015-10-31 23:08:50 +1100

    Redefine action types

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -1,2 +1,7 @@
-export const CLEAR_SELECTED = 'CLEAR_SELECTED';
+export const START_TIMER = 'START_TIMER';
+export const STOP_TIMER = 'STOP_TIMER';
+export const TIMER_TICK = 'TIMER_TICK';
+
+export const TOUCH_TILE = 'TOUCH_TILE';
+export const CLEAR_ATTEMPT = 'CLEAR_ATTEMPT';
 export const SUBMIT_ATTEMPT = 'SUBMIT_ATTEMPT';
