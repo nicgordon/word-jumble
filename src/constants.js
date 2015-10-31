bbd02b26e22057ed84264b77c389db89d2e3f1f1
Nic Gordon <gordonna@gmail.com>
2015-10-31 23:11:04 +1100

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const TILE_MOVEMENT = {
+  ADD_TO_ATTEMPT: 'ADD_TO_ATTEMPT',
+  RETURN_TO_JUMBLE: 'RETURN_TO_JUMBLE',
+};
+
+export const TILE_PANEL_TYPE = {
+  ATTEMPT: 'ATTEMPT',
+  JUMBLE: 'JUMBLE',
+};
+
+export const TIMER_STATE = {
+  OFF: 0,
+  ON: 1,
+};
