a512798dc88948e23dfd59608158688c29106a97
Nic Gordon <gordonna@gmail.com>
2015-10-31 23:10:48 +1100

    Export combined reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import score from './score';
 import tiles from './tiles';
+import time from './time';
 
 const rootReducer = combineReducers({
-  tiles
+  score,
+  time,
+  tiles,
 });
 
 export default rootReducer;
