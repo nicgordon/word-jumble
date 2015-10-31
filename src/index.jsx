710ee64f3c2fd41b25e2e99209305f58cb917b55
Nic Gordon <gordonna@gmail.com>
2015-10-31 23:56:13 +1100

    Setup dev-tools

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,19 @@ import { Provider } from 'react-redux';
 import Application from './containers/application';
 import configureStore from './store/configure-store';
 
+// React components for Redux DevTools
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Application />
-  </Provider>,
-  document.getElementById('root')
+ReactDOM.render((
+  <div>
+    <Provider store={store}>
+      <Application />
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>
+  ), document.getElementById('root')
 );
--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,16 +1,26 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers';
+import { compose, createStore } from 'redux'
+import rootReducer from '../reducers'
+
+// Redux DevTools store enhancers
+import { devTools, persistState } from 'redux-devtools';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const finalCreateStore = compose(
+    // Provides support for DevTools:
+    devTools(),
+    // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+  )(createStore);
 
-  // if (module.hot) {
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('../reducers', () => {
-  //     const nextReducer = require('../reducers');
-  //     store.replaceReducer(nextReducer);
-  //   });
-  // }
+  const store = finalCreateStore(rootReducer, initialState);
 
-  return store;
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers')
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
 }
