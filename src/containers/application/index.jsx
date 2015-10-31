1210c901e8e22f360c237473d45cfc4710bb72c3
Nic Gordon <gordonna@gmail.com>
2015-11-01 10:14:26 +1100

    Wire up keyboard listener

--- src/containers/application/index.jsx
+++ src/containers/application/index.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import pluck from 'lodash/collection/pluck';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { TILE_PANEL_TYPE } from '../../constants';
+import { TILE_MOVEMENT, TILE_PANEL_TYPE } from '../../constants';
 
 // Actions
 import tilesActions from '../../actions/tiles';
@@ -22,16 +22,61 @@ export default class Application extends React.Component {
     super(props);
 
     this.onClearHandler = this.onClearHandler.bind(this);
+    this.onKeydownHandler = this.onKeydownHandler.bind(this);
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.onKeydownHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onKeydownHandler);
   }
 
   onClearHandler() {
     this.props.actions.tiles.clearAttempt();
   }
 
+  onKeydownHandler(event) {
+    event.preventDefault();
+    const { attempt, jumble } = this.props;
+
+    const keyCode = event.keyCode || event.which;
+    if (!keyCode) { return; }
+
+    switch(keyCode) {
+      case 13: // Enter
+        if (this.props.attempt.length > 0) {
+          this.submitAttempt();    
+        }
+        break;
+      case 8: // Backspace
+        if (this.props.attempt.length > 0) {
+          this.props.actions.tiles.touchTile(this.props.attempt.length - 1, TILE_MOVEMENT.RETURN_TO_JUMBLE);
+        }
+        break;
+      default:
+        const characterPressed = String.fromCharCode(keyCode);
+
+        // Loop through the jumble and see if there are any tiles with values the same as the key pressed
+        for (let i = 0, len = jumble.length; i < len; i++) {
+          const tile = jumble[i];
+
+          if (tile.value.toLowerCase() === characterPressed.toLowerCase()) {
+            this.props.actions.tiles.touchTile(i, TILE_MOVEMENT.ADD_TO_ATTEMPT);
+            break;
+          }
+        }
+    }
+  }
+
   onSubmitHandler(event) {
     event.preventDefault();
+    this.submitAttempt();
+  }
 
+  submitAttempt() {
     const submission = pluck(this.props.attempt, 'value').join('');
     this.props.actions.tiles.submitAttempt(submission);
   }
