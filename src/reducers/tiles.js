04555f9b896878aba6cc2f9c05b0118c7d540101
Nic Gordon <gordonna@gmail.com>
2015-10-31 23:10:29 +1100

    Added tiles reducer

--- src/reducers/tiles.js
+++ src/reducers/tiles.js
@@ -1,4 +1,5 @@
-import { CLEAR_SELECTED } from '../actions/action-types';
+import { TOUCH_TILE, CLEAR_ATTEMPT, SUBMIT_ATTEMPT } from '../actions/action-types';
+import { TILE_MOVEMENT } from '../constants';
 
 const initialState = {
   jumble: [
@@ -10,14 +11,70 @@ const initialState = {
     { value: 'e' },
     { value: 'n' },
   ],
-  solution: [],
+  attempt: [],
+
+  // TODO: Remove this
+  testing: null,
 };
 
-export default function todos(state = initialState, action) {
-  switch (action.type) {
-    case CLEAR_SELECTED:
-      return state;
-    default:
-      return state;
+const tilePlaceholder = { value: '' };
+
+function returnAllTilesToJumble(attempt, jumble) {
+  attempt.forEach(tile => {
+    returnTileToJumble(tile, jumble);
+  });
+  attempt = [];
+}
+
+function returnTileToJumble(tile, jumble) {
+  // Loop through the jumble until we find a placeholder and replace it with the tile
+  for (let i = 0, len = jumble.length; i < len; i++) {
+    const jumbleTile = jumble[i];
+
+    if (jumbleTile.value === '') {
+      jumble.splice(i, 1, touchedTile);
+      break;
+    }
   }
+}
+
+export default function time(state = initialState, action = {}) {
+  const reducers = {
+    [TOUCH_TILE]: () => {
+
+      console.log('action', action);
+
+      const newState = { ...state };
+      if (action.movement === TILE_MOVEMENT.ADD_TO_ATTEMPT) {
+        // Grab the touched tile from the jumble and replace it with a placeholder
+        const touchedTile = newState.jumble.splice(action.index, 1, { ...tilePlaceholder })[0];
+        // Append the touched tile to the attempt
+        newState.attempt.push(touchedTile);
+      } else {
+        // Grab the touched tile from the attempt
+        const touchedTile = newState.attempt.splice(action.index, 1)[0];
+        returnTileToJumble(touchedTile, newState.jumble);
+      }
+
+      console.log('newState', newState);
+
+      return newState;
+    },
+    [CLEAR_ATTEMPT]: () => {
+      const newState = { ...state };
+      returnAllTilesToJumble(newState.attempt, newState.jumble);
+
+      // TODO: Remove this, just testing
+      newState.testing = 'balls';
+
+      return newState;
+    },
+    [SUBMIT_ATTEMPT]: () => {
+      const newState = { ...state };
+      returnAllTilesToJumble(newState.attempt, newState.jumble);
+      return newState;
+    },
+  };
+
+  return reducers[action.type] ? reducers[action.type]() : state;
 }
